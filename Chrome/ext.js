6c02354bca0d7b6a2f772fb2b78cda9691f3de02
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2020-01-13 21:40:18 -0500

    Create ext.js

---
+++ Chrome/ext.js
@@ -0,0 +1,22 @@
+selection = window.getSelection()
+range = selection.getRangeAt(0)
+
+// reemplazar el substring seleccionado 1 
+range.startContainer.parentElement.innerHTML = range
+    .startContainer
+    .parentElement
+    .innerHTML
+    .replace(range.startContainer.substringData(
+        range.startOffset, range.endOffset - range.startOffset), 
+        ´<span style='background-color:yellow;'>${range.startContainer.substringData(range.startOffset, range.endOffset - range.startOffset)}</span>´
+    )            
+
+// reemplazar el substring seleccionado 2
+range.endContainer.parentElement.innerHTML = range
+    .endContainer
+    .parentElement
+    .innerHTML
+    .replace(range.endContainer.substringData(
+        range.startOffset, range.endOffset - range.startOffset), 
+        ´<span style='background-color:yellow;'>${range.endContainer.substringData(range.startOffset, range.endOffset - range.startOffset)}</span>´
+    )   
