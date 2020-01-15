655b5ede0bc4d05c04a5fa799c16ef7a58df9302
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2020-01-15 16:25:35 -0500

    Update ext.js

--- Chrome/ext.js
+++ Chrome/ext.js
@@ -1,6 +1,10 @@
 selection = window.getSelection()
 range = selection.getRangeAt(0)
 
+// obtener el indice del nodo seleccionado (solo el endContainer y startContainer de momento)
+var node = range.endContainer;
+for (var nodeIndex=0; (node=node.previousSibling); nodeIndex++);
+
 // reemplazar el substring seleccionado 1 
 range.startContainer.parentNode.innerHTML = range
     .startContainer
